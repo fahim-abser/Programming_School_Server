520fd3e11b53a68a6df81c2b31be77779e42462c
fahim-abser <fahimabser0123@gmail.com>
2022-11-02 19:45:35 +0600

    first commit

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 5000
-const cors = require ('cors')
-app.use(cors())
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+
 const courses = require('./data/courses.json')
+app.use(cors());
 app.get('/', (req, res) => {
   res.send('API Running')
 })
-app.get('/courses-categories',(req,res)=>{
-    res.send(courses)
+app.get('/courses', (req, res) => {
+  res.send(courses)
 })
-app.get('/courses-categories/:id',(req, res)=>{
+app.get('/courses/:id', (req, res) => {
   const id = req.params.id;
-  const category_course = courses-categories.filter(c=>c.course===id);
-  res.send(category_course);
+  const categoryCourse = courses.find(c => c.id == id);
+  res.send(categoryCourse);
 })
 
 app.listen(port, () => {
